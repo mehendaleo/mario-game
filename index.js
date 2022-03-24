3aa37723f18a9fc92d7a886f5f8c6ecdacf92fa4
mehendaleo <mehendaleomkar@gmail.com>
2022-03-24 13:54:33 -0400

    moved css to new file, added head and script/style links

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const gravity = 0.3;
+
 class Player {
     // constructor for player class, need starting position and dimensions (square to start off), and y velocity to represent gravity
     constructor() {
@@ -15,7 +17,7 @@ class Player {
         this.height = 30
         this.velocity = {
             x: 0,
-            y: 1
+            y: 0
         }
     }
 
@@ -28,6 +30,12 @@ class Player {
     updateLocation() {
         this.drawPlayer()
         this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
+        }
     }
 }
 
